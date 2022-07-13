7f48938f788940ec3cad62d3f6a63b6fac432f7f
Thomaz <tjhonnar@gmail.com>
2022-07-13 04:17:18 -0500

    v0.3

--- src/components/AgeCalculator.js
+++ src/components/AgeCalculator.js
@@ -1,49 +1,6 @@
-import { useState, useEffect } from 'react';
-import dayjs from 'dayjs';
+import { useState } from 'react';
 /* STYLES */
 import styles from '../styles/AgeCalculator.module.css'
-import { render } from 'react-dom';
-
-/* export default function AgeCalculator() {
-
-    const [numbers, setNumbers] = useState({
-        number1: "",
-    });
-
-    const handleInputChange = e => {
-        setNumbers({
-            ...numbers,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    const send = e => {
-        e.preventDefault()
-        const n1 = parseInt(numbers.number1);
-        const add = n1 + 5;
-        const nuevo = {
-            result: add,
-            valor1: n1
-        }
-        document.getElementById('result').innerHTML = nuevo.result;
-        console.log('Enviando datos...' + nuevo.result);
-    }
-    
-    return (
-        <>
-            <form onSubmit={send}>
-                <input
-                    type="number"
-                    name='number1'
-                    onChange={handleInputChange}
-                />
-                <button type='submit'>Calculate</button>
-            </form>
-            <p>{numbers.number1} + 5 = <span id='result'></span></p>
-            
-        </>
-    )
-} */
 
 export default function AgeCalculator() {
 
@@ -106,57 +63,3 @@ export default function AgeCalculator() {
         </>
     )
 }
-
-
-
-/* export default function AgeCalculator() {
-
-    const [date, setDate] = useState();
-
-    useEffect(() => {
-        const birth = new Date(date);
-        setDate(birth);
-        
-        return() => {
-            birth();
-            console.log('Cleanup');
-        }
-    }, []);
-
-    return (
-        <>
-            <h1>¡Coloca tu fecha de nacimiento!</h1>
-            <input
-                type="date"
-                name='date'
-                autoComplete='off'
-                onChange={ev => setDate(ev.target.value)}
-            />
-            <button>Calcular</button>
-
-            <p>{birth}</p>
-            <p></p>
-        </>
-    )
-} */
-
-/* const useCounter = initial => {
-    const [ counter, setCounter ] = useState(initial);
-
-    const add = () => {
-        setCounter(counter + 1);
-    }
-    return { counter, add }
-}
-
-export default function AgeCalculator() {
-
-    const { counter, add } = useCounter(0)
-
-    return(
-        <div>
-            <p>{counter}</p>
-            <button onClick={add}>Add</button>
-        </div>
-    )
-} */
